5573502350e07ecf0ddaa628090bdb1520744927
Eliseo Gomez <jose.eliseo.gomez@gmail.com>
2022-05-11 08:43:23 -0600

    adding strcuture and protocol

--- Week 1/Reading/SwiftTour.playground/Contents.swift
+++ Week 1/Reading/SwiftTour.playground/Contents.swift
@@ -405,3 +405,40 @@ struct Card {
 }
 let threeOfSpades = Card(rank: .three, suit: .spades)
 let threeOfSpadesDescription = threeOfSpades.simpleDescription()
+
+
+
+
+
+
+protocol ExampleProtocol {
+    var simpleDescription: String {get}
+    mutating func adjust()
+}
+
+class SimpleClass: ExampleProtocol {
+    var simpleDescription: String = "A very simple class."
+    var anotherProperty: Int = 69105
+    func adjust() {
+        simpleDescription += "  Now 100% adjusted."
+    }
+}
+
+
+var a = SimpleClass()
+
+a.adjust()
+let aDescription = a.simpleDescription
+
+struct SimpleStructure: ExampleProtocol{
+    var simpleDescription: String = "A simple Structure"
+    mutating func adjust() {
+        simpleDescription = " (adjusted) "
+    }
+}
+
+var b = SimpleStructure()
+b.adjust()
+let bDescriptino = b.simpleDescription
+
+
