c3c32c6664d8f65c5d4c1267890544376039cac8
Eliseo Gomez <jose.eliseo.gomez@gmail.com>
2022-06-24 10:55:11 -0600

    pie class

--- Week 1/Memorize/Pie.swift
+++ Week 1/Memorize/Pie.swift
@@ -7,4 +7,24 @@
 
 import SwiftUI
 
+struct Pie: Shape {
+    var startAngle: Angle
+    var endAngle: Angle
+    
+    func path(in rect: CGRect) -> Path {
+        
+        let center = CGPoint(x: rect.midX, y: rect.midY)
+        let radious = min(rect.width, rect.height) / 2
+        
+        let start = CGPoint(
+            x: center.x + radious * CGFloat(cos(startAngle.radians)),
+            y: center.y + radious * CGFloat(sin(startAngle.radians)
+        )
+        
+        var p = Path()
+        p.move(to: center)
+        
+        return p
+    }
+}
 
